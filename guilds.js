5c40f20ed462d288698e6b08db47b247eb0b37fc
Viceerpp <pcgafa@gmail.com>
2026-07-08 15:32:16 +0300

    Add files via upload

--- guilds.js
+++ guilds.js
@@ -1,93 +1,92 @@
 window.activeCommunities = [
   {
     "name": "▽ Harbi Cream ▽",
-    "members": 148,
-    "iconUrl": "https://cdn.discordapp.com/icons/617332842296508456/1c9032aa5416895d3eb81fc06bfca6de.png",
-    "plays": 472,
-    "listenHours": 192
+    "members": 147,
+    "iconUrl": "https://cdn.discordapp.com/icons/617332842296508456/1c9032aa5416895d3eb81fc06bfca6de.png"
+  },
+  {
+    "name": "frauds",
+    "members": 3,
+    "iconUrl": "https://cdn.discordapp.com/icons/888495897263087626/d781cc281d45a91fc492baa421abda05.png"
   },
   {
     "name": "👹𝔇𝔦𝔞𝔟𝔩𝔬 𝔉𝔞𝔪𝔦𝔩𝔶👹",
     "members": 29,
-    "iconUrl": "https://cdn.discordapp.com/icons/939600419322216529/dcb1029577981ed0b5d3c051c6eae16f.png",
-    "plays": 150,
-    "listenHours": 36
+    "iconUrl": "https://cdn.discordapp.com/icons/939600419322216529/dcb1029577981ed0b5d3c051c6eae16f.png"
   },
   {
     "name": "𝙑𝙞𝙘𝙚",
-    "members": 803,
-    "iconUrl": "https://cdn.discordapp.com/icons/1181263885488107560/a6bbecbd984da0c84f9cb9a3a59cfe67.png",
-    "plays": 9420,
-    "listenHours": 960
+    "members": 804,
+    "iconUrl": "https://cdn.discordapp.com/icons/1181263885488107560/a6bbecbd984da0c84f9cb9a3a59cfe67.png"
   },
   {
     "name": "𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐉𝐨𝐢𝐧💎𝐉𝟒𝐉 #𝐆𝐋𝐎𝐁𝐀𝐋💎",
     "members": 103,
-    "iconUrl": "https://cdn.discordapp.com/icons/1190713407553425428/820b6a14950ed3b58785e3247c678643.png",
-    "plays": 300,
-    "listenHours": 120
+    "iconUrl": "https://cdn.discordapp.com/icons/1190713407553425428/820b6a14950ed3b58785e3247c678643.png"
   },
   {
     "name": "j4j",
     "members": 2,
-    "iconUrl": "https://cdn.discordapp.com/icons/1191012453052592160/c58c15cd213506c2dadb9cc563524a8f.png",
-    "plays": 5,
-    "listenHours": 1
+    "iconUrl": "https://cdn.discordapp.com/icons/1191012453052592160/c58c15cd213506c2dadb9cc563524a8f.png"
   },
   {
     "name": "𝙑𝙞𝙘𝙚 𝙍𝙥",
     "members": 30,
-    "iconUrl": "https://cdn.discordapp.com/icons/1191461875788025947/fafffda94d9cc3e37832d7f516ccc5a7.png",
-    "plays": 250,
-    "listenHours": 48
+    "iconUrl": "https://cdn.discordapp.com/icons/1191461875788025947/fafffda94d9cc3e37832d7f516ccc5a7.png"
   },
   {
     "name": "𝐕𝐢𝐜𝐞 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐦𝐞𝐧𝐭",
     "members": 10,
-    "iconUrl": "https://cdn.discordapp.com/icons/1238933352288092191/e06c127ba5af8928ed33bee888e469d8.png",
-    "plays": 80,
-    "listenHours": 24
+    "iconUrl": "https://cdn.discordapp.com/icons/1238933352288092191/e06c127ba5af8928ed33bee888e469d8.png"
   },
   {
     "name": "sohbet sunucu etmek",
     "members": 7,
-    "iconUrl": "https://cdn.discordapp.com/icons/1267142123229286463/df8d13db4740c598d6efe1f6c4946847.png",
-    "plays": 50,
-    "listenHours": 12
-  },
-  {
-    "name": "Mert kullanıcısının sunucusu",
-    "members": 2,
-    "iconUrl": "https://cdn.discordapp.com/icons/1395832288427769896/3f42ffc858285657f64a81b0c74366b8.png",
-    "plays": 3,
-    "listenHours": 1
-  },
-  {
-    "name": "KULUKU kullanıcısının sunucusu",
-    "members": 3,
-    "iconUrl": "",
-    "plays": 10,
-    "listenHours": 2
-  },
-  {
-    "name": "League Of Legends Duo Merkezi",
-    "members": 7,
-    "iconUrl": "https://cdn.discordapp.com/icons/1472692301443764304/5326332829323d9d00ac80a5355030cd.png",
-    "plays": 40,
-    "listenHours": 8
-  },
-  {
-    "name": "ARCHİGE GİBİ AVELLER İÇİN TİCKET BOT",
-    "members": 4,
-    "iconUrl": "",
-    "plays": 30,
-    "listenHours": 5
+    "iconUrl": "https://cdn.discordapp.com/icons/1267142123229286463/df8d13db4740c598d6efe1f6c4946847.png"
   },
   {
     "name": "lactarius kullanıcısının sunucusu",
     "members": 10,
-    "iconUrl": "",
-    "plays": 45,
-    "listenHours": 10
+    "iconUrl": ""
+  },
+  {
+    "name": "Mert kullanıcısının sunucusu",
+    "members": 2,
+    "iconUrl": "https://cdn.discordapp.com/icons/1395832288427769896/3f42ffc858285657f64a81b0c74366b8.png"
+  },
+  {
+    "name": "root test sistemleri",
+    "members": 5,
+    "iconUrl": "https://cdn.discordapp.com/icons/1441535303730069679/c1d79ae1a6bc12b6028de9018ebaf008.png"
+  },
+  {
+    "name": "ARCHİGE GİBİ AVELLER İÇİN TİCKET BOT",
+    "members": 4,
+    "iconUrl": ""
+  },
+  {
+    "name": "Finlandiya",
+    "members": 29,
+    "iconUrl": "https://cdn.discordapp.com/icons/1462137728912064746/d21ee60f74a8391e4500a7fe73c53a6a.png"
+  },
+  {
+    "name": "- TÜRKİYE - ADALET VE KALKINMA PARTİSİ",
+    "members": 11,
+    "iconUrl": "https://cdn.discordapp.com/icons/1469096120566284414/5b9b2c889b054742fa17b0fb2577727e.png"
+  },
+  {
+    "name": "KULUKU kullanıcısının sunucusu",
+    "members": 3,
+    "iconUrl": ""
+  },
+  {
+    "name": "League Of Legends Duo Merkezi",
+    "members": 7,
+    "iconUrl": "https://cdn.discordapp.com/icons/1472692301443764304/5326332829323d9d00ac80a5355030cd.png"
+  },
+  {
+    "name": "muzik",
+    "members": 3,
+    "iconUrl": "https://cdn.discordapp.com/icons/1524131937516785827/38940e4bc4c4889a7844ded00fcf3249.png"
   }
 ];
